d6c792fc83cb2c5650fb1e0c2a6dee729aecabb2
Guilherme Valgas <guilhermehvalgas@gmail.com>
2022-09-21 18:41:25 -0300

    Exercise-3 done

---
+++ fundamentos/secao-04-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/Exercicios-do-dia/exercise-3.js
@@ -0,0 +1,13 @@
+const a = 13;
+const b = 12;
+const c = 22;
+
+if (a > b && a > c) {
+  console.log('Número ' + a + ' é maior!')
+} 
+else if (b > c) {
+  console.log('Número ' + b + ' é maior!')
+}
+else {
+  console.log('Número ' + c + ' é maior!')
+}
