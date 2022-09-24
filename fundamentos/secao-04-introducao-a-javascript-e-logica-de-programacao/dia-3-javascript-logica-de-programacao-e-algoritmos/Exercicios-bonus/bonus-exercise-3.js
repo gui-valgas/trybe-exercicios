967e694e5ea8553e9f2f18da21e43ace03f90871
Guilherme Valgas <guilhermehvalgas@gmail.com>
2022-09-23 21:19:03 -0300

    small fix

--- fundamentos/secao-04-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/Exercicios-bonus/bonus-exercise-3.js
+++ fundamentos/secao-04-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/Exercicios-bonus/bonus-exercise-3.js
@@ -5,9 +5,7 @@ let resultado = '';
 let posicao = n;
 
 for (let index = 0; index < n; index += 1) {
-  
-  
-    for (let index2 = 0; index2 < n; index2 += 1) {
+  for (let index2 = 0; index2 < n; index2 += 1) {
      if (index2 < posicao - 1) {
         resultado += ' '
     } else {
