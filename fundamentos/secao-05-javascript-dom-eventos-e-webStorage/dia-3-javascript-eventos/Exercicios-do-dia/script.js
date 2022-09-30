238193b52219a9e35462050d60703190382b4691
Guilherme Valgas <guilhermehvalgas@gmail.com>
2022-09-30 19:35:12 -0300

    [task 1] Done

--- fundamentos/secao-05-javascript-dom-eventos-e-webStorage/dia-3-javascript-eventos/Exercicios-do-dia/script.js
+++ fundamentos/secao-05-javascript-dom-eventos-e-webStorage/dia-3-javascript-eventos/Exercicios-do-dia/script.js
@@ -14,3 +14,35 @@ function createDaysOfTheWeek() {
 createDaysOfTheWeek();
 
 // Escreva seu código abaixo.
+
+let decemberDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+
+function criaDiasDoMes() {
+  let listaDosDias = document.querySelector('#days');
+
+  for (let index = 0; index < decemberDaysList.length; index += 1) {
+    let dia = decemberDaysList[index];
+    let itemDia = document.createElement('li');
+    itemDia.innerHTML = dia;
+
+    if (dia === 24 || dia === 31) {
+      itemDia.className = 'day holiday';
+      listaDosDias.appendChild(itemDia)
+    } else if ( dia === 4 || dia === 11 || dia === 18){
+      itemDia.className = 'day friday';
+      listaDosDias.appendChild(itemDia)
+    } else if ( dia === 25) {
+      itemDia.className = 'day friday holiday';
+      listaDosDias.appendChild(itemDia)
+    } else {
+      itemDia.className = 'day'
+      listaDosDias.appendChild(itemDia)
+    }
+}
+}
+
+criaDiasDoMes();
+
+function criaBotaoFeriados(feriados) {
+
+}
