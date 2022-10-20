62836ff3556b2ded9032002c51b96b1452687a26
Guilherme Valgas <guilhermehvalgas@gmail.com>
2022-10-19 22:11:31 -0300

    exercicio 4 ok

---
+++ fundamentos/secao-07-introducao-javascriptES6-e-testes-unitarios/dia-03-primeiros-passos-em-Jest/Exercicios-do-dia/encodeDecode.test.js
@@ -0,0 +1,15 @@
+const { encode, decode } = require('./encodeDecode.js')
+
+describe('teste da função encode e decode', () => {
+  test('verifica se encode e decode são funções', () => {
+    expect(typeof encode).toEqual('function');
+    expect(typeof decode).toEqual('function');
+  })
+  test('verifica se na função encode a,e,i,o,u são convertidas em 1, 2, 3, 4, 5 e na função decode 1, 2, 3, 4, 5 em a, e, i, o, u', () => {
+    expect(encode('aurelio')).toEqual('15r2l34')
+    expect(decode('15r2l34')).toEqual('aurelio')
+  })
+  test('verifica o retorno do mesmo número de caracteres no retorno da função', () => {
+    expect(encode('aurelio').length).toEqual(7);
+  })
+})
