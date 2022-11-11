c8626ab9f805d03d1e67d8c3a8ecaf99ca244345
Guilherme Valgas <guilhermehvalgas@gmail.com>
2022-11-11 19:18:45 -0300

    Exercício de fixação dia-02

--- front-end/secao-01-introducao-ao-frontEnd-e-JavaScript-assincrono/dia-2-javascript-assincrono-promises-e-fetch/Exercicios-de-fixacao/index.js
+++ front-end/secao-01-introducao-ao-frontEnd-e-JavaScript-assincrono/dia-2-javascript-assincrono-promises-e-fetch/Exercicios-de-fixacao/index.js
@@ -14,19 +14,33 @@ const usersSelect = document.querySelector('#users-select');
 const USERS_API = 'https://dummyjson.com/users';
 fetch(USERS_API)
   .then((response) => {
-    console.log(response)
-      return response.json()
+    return response.json()
   })
   .then((data) => {
-    console.log(data)
-    const { users } = data;
-    console.log(users)
+    const users = data.users;
 
     // Chama a função auxiliar para preencher os nomes e ids no select users-select
     fillUsersSelect(users);
+
   });
 usersSelect.addEventListener('change', () => {
   clearPageData();
 
-  // faça a lógica para pegar as informações dos posts da pessoa selecionada e dos comentários do post destacado aqui.
+  const posts = fetch(`https://dummyjson.com/posts/user/${usersSelect.value}`)
+    .then((response) => response.json())
+    .then((data) => {
+      const postDestacado = data.posts[0]
+      fillPosts(data.posts)
+
+      const commentsAPI = `https://dummyjson.com/posts/${postDestacado.id}/comments`
+
+      return fetch(commentsAPI)
+    }).then((response) => response.json())
+    .then((data) => {
+      const comments = data.comments
+      fillFeaturedPostComments(comments)
+    })
+    .catch((error) => {
+      fillErrorMessage('Error ao recuperar posts e comentários');
+    })
 });
