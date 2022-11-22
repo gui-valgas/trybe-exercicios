5b35a055870c1595b2685a0755e42acf9dbd6f3e
Guilherme Valgas <guilhermehvalgas@gmail.com>
2022-11-22 20:12:44 -0300

    Exercicios seção2.1

---
+++ front-end/secao-02-introducao-ao-React/dia-1-hello-world-no-React/Exercicios-de-fixacao/src/About.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+class About extends React.Component {
+  render() {
+    return (
+      <body>
+        <div>
+          <h1>
+            Guilherme Valgas
+          </h1>
+          <p>
+            Sou de Belo Horizonte - MG, e tenho 26 anos.
+          </p>
+          <h2>
+            Minhas habilidades:
+          </h2>
+          <ul>
+            <li>JavaScript</li>
+            <li>Comunicação</li>
+            <li>CSS</li>
+          </ul>
+        </div>
+      </body>
+    )
+  }
+}
+
+export default About;
---
+++ front-end/secao-02-introducao-ao-React/dia-1-hello-world-no-React/Exercicios-do-dia/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+import Header from './Header';
+import Content from './Content';
+import Footer from './Footer';
+
+function App() {
+  return (
+    <body>
+      <div>
+        <Header />
+        <Content />
+        <Footer />
+      </div>
+    </body>
+  );
+}
+
+export default App;
