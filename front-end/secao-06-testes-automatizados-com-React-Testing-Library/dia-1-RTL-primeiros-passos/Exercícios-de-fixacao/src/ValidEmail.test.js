6a7270c3943f8fd3bc4c45aad5c3cdebc7b96cbe
Guilherme Valgas <guilhermehvalgas@gmail.com>
2023-01-17 18:57:54 -0300

    Exercicios de fixação

---
+++ front-end/secao-06-testes-automatizados-com-React-Testing-Library/dia-1-RTL-primeiros-passos/Exercícios-de-fixacao/src/ValidEmail.test.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import ValidEmail from './ValidEmail';
+
+test('Testando um componente, caso o email seja válido.', () => {
+  const EMAIL_USER = 'email@email.com';
+  render(<ValidEmail email={ EMAIL_USER } />);
+  const isValid = screen.getByText('Email Válido');
+  expect(isValid).toBeInTheDocument();
+});
+
+test('Testando um componente, caso o email seja inválido.', () => {
+  const EMAIL_USER = 'emailerrado';
+  render(<ValidEmail email={ EMAIL_USER } />);
+  const isValid = screen.getByText('Email Inválido');
+  expect(isValid).toBeInTheDocument();
+});
+
+test('Testa caso o campo e-mail esteja vazio o componente não é renderizado', () => {
+  render(<ValidEmail email="" />)
+  const isValidEmail = screen.queryByTestId('id-is-email-valid')
+  expect(isValidEmail).not.toBeInTheDocument();
+})
+
+test('Testando se o componente possui texto verde ao ser digitado um e-mail válido.', () => {
+  const EMAIL_USER = 'email@email.com';
+
+  render(<ValidEmail email={EMAIL_USER} />);
+  const isValidText = screen.getByTestId('id-is-email-valid');
+  expect(isValidText).toHaveAttribute('class', 'green-text');
+});
+
+test('Testando se o componente possui texto vermelho ao ser digitado um e-mail inválido.', () => {
+  const EMAIL_USER = 'emailerrado';
+
+  render(<ValidEmail email={EMAIL_USER} />);
+  const isValidText = screen.getByTestId('id-is-email-valid');
+  expect(isValidText).toHaveAttribute('class', 'red-text');
+});
