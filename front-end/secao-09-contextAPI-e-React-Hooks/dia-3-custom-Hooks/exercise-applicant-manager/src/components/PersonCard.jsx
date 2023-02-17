f518062ebf4a0d3dcbb7372df22e658ba3ea14ac
Guilherme Valgas <guilhermehvalgas@gmail.com>
2023-02-17 13:54:10 -0300

    daily exercises

---
+++ front-end/secao-09-contextAPI-e-React-Hooks/dia-3-custom-Hooks/exercise-applicant-manager/src/components/PersonCard.jsx
@@ -0,0 +1,14 @@
+import PropTypes from 'prop-types';
+
+export default function PersonCard(props) {
+  const { person: { name, picture } } = props;
+
+  return (
+    <figure>
+      <figcaption>
+        <h2>{ `${name.first} ${name.last}` }</h2>
+      </figcaption>
+      <img src={ picture.large } alt="thumbnail" />
+    </figure>
+  );
+}
