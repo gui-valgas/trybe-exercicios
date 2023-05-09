1829b8a6997da36fd6e5a693640d8ece902c6a41
Guilherme Valgas <guilhermehvalgas@gmail.com>
2023-05-09 13:56:46 -0300

    Exercícios dia 3 - DONE

---
+++ back-end/secao-06-Nodejs-ORM-autenticacao/dia-3-ORM-associations-N:N-e-transactions/daily-exercises/src/controllers/plansController.js
@@ -0,0 +1,19 @@
+const service = require('../services/plansService');
+
+const getAllPlans = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const listOfPlans = await service.getAllPlans(id);
+
+    if (listOfPlans.message) return res.status(404).send(listOfPlans);
+
+    return res.status(200).json(listOfPlans);
+  } catch (e) {
+    console.log(e);
+    return res.status(500).json({ message: 'Algo deu errado' });
+  }
+};
+
+module.exports = {
+  getAllPlans,
+};
