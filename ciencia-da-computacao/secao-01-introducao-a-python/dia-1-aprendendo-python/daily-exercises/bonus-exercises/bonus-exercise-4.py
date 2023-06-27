cd4b4c8d95a1860276169bc3f443cca5db280b72
Guilherme Valgas <guilhermehvalgas@gmail.com>
2023-06-27 20:15:28 -0300

    smal fix

--- ciencia-da-computacao/secao-01-introducao-a-python/dia-1-aprendendo-python/daily-exercises/bonus-exercises/bonus-exercise-4.py
+++ ciencia-da-computacao/secao-01-introducao-a-python/dia-1-aprendendo-python/daily-exercises/bonus-exercises/bonus-exercise-4.py
@@ -1,10 +1,10 @@
 def fuel_price(gas, liters):
-    if gas == 'A':
+    if gas == "A":
         price = 1.90
         discount = 0.03
         if liters > 20:
             discount = 0.05
-    elif gas == 'G':
+    elif gas == "G":
         price = 2.50
         discount = 0.04
         if liters > 20:
