3e9d2d8c8451fa55a330fe991882b3816461be38
Guilherme Valgas <guilhermehvalgas@gmail.com>
2023-06-27 19:57:02 -0300

    Exercícios do dia 1.1 - ciência da computação

---
+++ ciencia-da-computacao/secao-01-introducao-a-python/dia-1-aprendendo-python/daily-exercises/exercises/exercise-5.py
@@ -0,0 +1,8 @@
+import math  # Utilizando o módulo math
+
+
+def paint_costs(area):
+    can_price = 80
+    required_liters = area / 3
+    required_cans = math.ceil(required_liters / 18)
+    return required_cans, required_cans * can_price
