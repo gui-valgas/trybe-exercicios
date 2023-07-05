f6f4032ebfa44cd6164715e11199eafcd0f26206
Guilherme Valgas <guilhermehvalgas@gmail.com>
2023-07-05 16:23:48 -0300

    Exercícios do dia 3 - Ciência da computação DONE!

---
+++ ciencia-da-computacao/secao-02-algoritmos/dia-3-algoritmos-de-ordenacao-e-busca/daily-exercises/exercise1.py
@@ -0,0 +1,6 @@
+# Dado um array com os seguintes elementos
+# ["zebra", "macaco", "elefante", "arara", "javali"],
+# após duas iterações  utilizando bubble sort, como estaria este array?
+
+"""Após duas iterações o array estaria da seguinte forma"""
+["elefante", "arara", "javali", "macaco", "zebra"]
